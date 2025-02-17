d771fe0f2fcd206b95284b68f5cb53554b4661c5
suphanan <ouengnoisuphanan@gmail.com>
2025-02-17 20:40:32 +0700

    แกไขไฟล server.php

--- server.php
+++ server.php
@@ -1,14 +1,14 @@
 <?php
 
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "project";
+    $servername = "lockingdevice.pcnone.com";  // เปลี่ยนจาก localhost เป็นโดเมนของคุณ
+    $username = "root";  // ชื่อผู้ใช้ที่ใช้เชื่อมต่อกับฐานข้อมูล
+    $password = "@Project67 ";  // รหัสผ่านของฐานข้อมูล ถ้ามี
+    $dbname = "project";  // ชื่อฐานข้อมูล
 
     $conn = mysqli_connect($servername, $username, $password, $dbname);
     
     if(!$conn){
-        die("Connection success" . mysqli_connect_error());
+        die("Connection failed: " . mysqli_connect_error());  // แก้ไขข้อความจาก "Connection success" เป็น "Connection failed"
     }
 
 ?>
