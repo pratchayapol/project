ed51eaea47005c9c2322ee5e7253d8e1806055b6
suphanan <ouengnoisuphanan@gmail.com>
2025-02-18 21:22:01 +0700

    comment

--- webhome.php
+++ webhome.php
@@ -8,7 +8,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&family=Inter:wght@400;600&display=swap" rel="stylesheet">
     <style>
         body {
-            font-family: 'Poppins', sans-serif;
+            font-family: 'Roboto', sans-serif;
             background: url('https://cdn.pixabay.com/photo/2019/04/10/11/56/watercolor-4116932_640.png') no-repeat center center fixed;
             background-size: cover;
         }
