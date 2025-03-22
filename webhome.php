db3d6db240b3aaa448c6ea2340d132028fa2deb1
suphanan <ouengnoisuphanan@gmail.com>
2025-03-22 19:31:49 +0700

    comment

--- webhome.php
+++ webhome.php
@@ -20,18 +20,18 @@
         <div class="flex items-center justify-center mb-6">
             <img src="https://i.postimg.cc/wTy8jZs0/download.jpg" alt="RMUTI Logo" class="w-24 h-auto">
         </div>
-        <h1 class="text-2xl text-gray-800 mb-6" style="font-family: 'Kanit', sans-serif;">ระบบบันทึกข้อมูลรถและสแกนจ่ายชำระค่าปรับ</h1>
+        <h1 class="text-xl text-gray-800 mb-6" style="font-family: 'Kanit', sans-serif;">ระบบบันทึกข้อมูลรถและสแกนจ่ายชำระค่าปรับ</h1>
         <div class="space-y-6">
             <!-- ปุ่มคู่มือสำหรับเจ้าหน้าที่ -->
             <a href="webboard.php" class="block">
-                <button class="w-full px-6 py-4 bg-gradient-to-r from-[#2B547E] to-[#29465B] text-white text-lg rounded-lg shadow-lg transform hover:scale-105 hover:shadow-2xl transition duration-300"
+                <button class="w-full px-6 py-4 bg-gradient-to-r from-[#2B547E] to-[#29465B] text-white text-base rounded-lg shadow-lg transform hover:scale-105 hover:shadow-2xl transition duration-300"
                 style="font-family: 'Kanit', sans-serif;">
                    ระบบบันทึกข้อมูลรถสำหรับเจ้าหน้าที่
                 </button>
             </a>
             <!-- ปุ่มคู่มือสำหรับผู้ชำระค่าปรับ -->
             <a href="home1.php" class="block">
-                <button class="w-full px-6 py-4 bg-gradient-to-r from-[#50C878] to-[#1B8A6B] text-white text-lg rounded-lg shadow-lg transform hover:scale-105 hover:shadow-2xl transition duration-300"
+                <button class="w-full px-6 py-4 bg-gradient-to-r from-[#50C878] to-[#1B8A6B] text-white text-base rounded-lg shadow-lg transform hover:scale-105 hover:shadow-2xl transition duration-300"
                 style="font-family: 'Kanit', sans-serif;">
                     ระบบสแกนจ่ายค่าปรับสำหรับผู้ชำระค่าปรับ
                 </button>
