8ad22d7f05a06c747786371606aae03897eb64da
suphanan <ouengnoisuphanan@gmail.com>
2025-03-23 17:18:26 +0700

    comment

--- chack_device.php
+++ chack_device.php
@@ -146,11 +146,11 @@ $result = $conn->query($sql);
             </button>
             <h1 class="text-xl" style="font-family: 'Kanit', sans-serif;">ตรวจเช็คอุปกรณ์ล็อกล้อ</h1>
             <div class="flex items-center space-x-6">
+            <a href="logout.php" class="block px-4 py-2 hover:bg-gray-100" style="font-family: 'Kanit', sans-serif;">Logout</a>
                 <div class="relative group cursor-pointer">
                     <img src="https://www.svgrepo.com/show/524199/user-circle.svg" alt="User Icon" class="h-6 w-6 group-hover:text-gray-200" style="filter: brightness(0) invert(1);">
                     <div class="absolute right-0 mt-2 w-40 bg-white text-gray-800 shadow-lg rounded-md opacity-0 group-hover:opacity-100 group-hover:translate-y-2 transition-all duration-200">
                         <a href="profile1.php" class="block px-4 py-2 hover:bg-gray-100" style="font-family: 'Kanit', sans-serif;">Profile</a>
-                        <a href="logout.php" class="block px-4 py-2 hover:bg-gray-100" style="font-family: 'Kanit', sans-serif;">Logout</a>
                     </div>
                 </div>
             </div>
